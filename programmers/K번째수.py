eea5dc7bdd2849c7474e37cf671cb3ed05793137
Kimyechan <vlvkcjswo7@naver.com>
2020-09-19 22:52:17 +0900

    Sorting: K번째수
    slicing and sorting

---
+++ programmers/K번째수.py
@@ -0,0 +1,12 @@
+def solution(array, commands):
+    answer = []
+    sliceArray = []
+
+    for command in commands:
+        sliceArray = array[command[0]-1:command[1]]
+        sliceArray.sort()
+        answer.append(sliceArray[command[2]-1])
+
+    return answer
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
