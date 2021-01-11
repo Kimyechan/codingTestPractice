ff22143d887605269ca29632539b7d99425f6483
Kimyechan <vlvkcjswo7@naver.com>
2021-01-11 14:00:09 +0900

    programmers 가장 큰 정사각형 찾기

---
+++ programmers/가장 큰 정사각형 찾기.py
@@ -0,0 +1,6 @@
+def solution(board):
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i-1][j], board[i][j-1], board[i-1][j-1]) + 1
+    return max([num for row in board for num in row]) ** 2
