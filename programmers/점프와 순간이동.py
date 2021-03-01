baea7a8e76a427a87ee4f411c20d27673af91ffc
Kimyechan <vlvkcjswo7@naver.com>
2021-03-01 23:48:36 +0900

    programmers 점프와 순간이동

---
+++ programmers/점프와 순간이동.py
@@ -0,0 +1,17 @@
+def solution(n):
+    ans = 0
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n - 1
+            ans += 1
+
+    ans = ans + 1
+
+    return ans
+
+# 다른 사람 풀이
+# def solution(n):
+#     return bin(n).count('1')
