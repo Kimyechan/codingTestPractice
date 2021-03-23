1423209315fa6e2912416ad0fe80a60fb9828a63
Kimyechan <vlvkcjswo7@naver.com>
2021-03-24 01:35:46 +0900

    Baekjun priority-queue 가운데를 말해요

---
+++ baekjun/priority-queue/가운데를 말해요.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+
+n = int(input())
+left = []
+right = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+
+    if len(left) == len(right):
+        heapq.heappush(left, (-num, num))
+    else:
+        heapq.heappush(right, (num, num))
+
+    if right and left[0][1] > right[0][1]:
+        leftMax = heapq.heappop(left)[1]
+        rightMin = heapq.heappop(right)[1]
+        heapq.heappush(left, (-rightMin, rightMin))
+        heapq.heappush(right, (leftMax, leftMax))
+
+    print(left[0][1])
