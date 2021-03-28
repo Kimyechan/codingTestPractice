1d2f5f35f68db1ec5c6dabe99c82880cb1b2e9a4
Kimyechan <vlvkcjswo7@naver.com>
2021-03-28 23:37:13 +0900

    Baekjun dp 계단 오르기

---
+++ baekjun/dp/계단 오르기.py
@@ -0,0 +1,21 @@
+import sys
+n = int(sys.stdin.readline())
+stairs = [0]
+for i in range(n):
+    stairs.append(int(sys.stdin.readline()))
+
+if n == 1:
+    print(stairs[1])
+elif n == 2:
+    print(stairs[1] + stairs[2])
+elif n == 3:
+    print(max(stairs[1] + stairs[3], stairs[2] + stairs[3]))
+else:
+    dp = [0] * (n + 1)
+    dp[1] = stairs[1]
+    dp[2] = stairs[1] + stairs[2]
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+    for i in range(4, n + 1):
+        dp[i] = max((dp[i-3] + stairs[i - 1] + stairs[i]), (dp[i - 2] + stairs[i]))
+    print(dp[n])
