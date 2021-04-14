7b58c4f3d52495b2e822a598cddbb0b297165b68
Kimyechan <vlvkcjswo7@naver.com>
2021-04-14 19:49:30 +0900

    programmers stack-queue resolve

--- programmers/고득점Kit복습/stack-queue/기능개발.py
+++ programmers/고득점Kit복습/stack-queue/기능개발.py
@@ -1,29 +1,80 @@
+# import math
+#
+#
+# def solution(progresses, speeds):
+#     answer = []
+#     days = []
+#     for i in range(len(progresses)):
+#         days.append(math.ceil((100 - progresses[i]) / speeds[i]))
+#
+#     stack = []
+#     for day in days:
+#         if len(stack) == 0:
+#             stack.append(day)
+#         else:
+#             count = 0
+#             while len(stack) != 0 and stack[-1] < day and stack[0] < day:
+#                 stack.pop()
+#                 count += 1
+#
+#             stack.append(day)
+#
+#             if count != 0:
+#                 answer.append(count)
+#
+#     answer.append(len(stack))
+#     return answer
+#
+#
+# print(solution([93, 30, 55], [1, 30, 5]))
 import math
 
 
 def solution(progresses, speeds):
     answer = []
-    days = []
-    for i in range(len(progresses)):
-        days.append(math.ceil((100 - progresses[i]) / speeds[i]))
 
+    needDays = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(len(progresses))]
     stack = []
-    for day in days:
+    for needDay in needDays:
         if len(stack) == 0:
-            stack.append(day)
+            stack.append(needDay)
         else:
-            count = 0
-            while len(stack) != 0 and stack[-1] < day and stack[0] < day:
+            deployCount = 0
+            while len(stack) != 0 and stack[0] < needDay:
                 stack.pop()
-                count += 1
+                deployCount += 1
+            stack.append(needDay)
+            if deployCount != 0:
+                answer.append(deployCount)
 
-            stack.append(day)
+    if len(stack) != 0:
+        answer.append(len(stack))
 
-            if count != 0:
-                answer.append(count)
-
-    answer.append(len(stack))
     return answer
 
 
 print(solution([93, 30, 55], [1, 30, 5]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- programmers/고득점Kit복습/stack-queue/다리를 지나는 트럭.py
+++ programmers/고득점Kit복습/stack-queue/다리를 지나는 트럭.py
@@ -21,27 +21,75 @@
 #     step += bridge_length
 #
 #     return step
+# from collections import deque
+#
+#
+# def solution(bridge_length, weight, truck_weights):
+#     bridgeQ = deque([0 for _ in range(bridge_length)])
+#     truckQ = deque(truck_weights)
+#
+#     totalWeight = 0
+#     totalTime = 0
+#
+#     while truckQ:
+#         totalWeight -= bridgeQ.popleft()
+#         if totalWeight + truckQ[0] <= weight:
+#             truckWeight = truckQ.popleft()
+#             totalWeight += truckWeight
+#             bridgeQ.append(truckWeight)
+#         else:
+#             bridgeQ.append(0)
+#
+#         totalTime += 1
+#
+#     totalTime += bridge_length
+#
+#     return totalTime
 from collections import deque
 
 
 def solution(bridge_length, weight, truck_weights):
-    bridgeQ = deque([0 for _ in range(bridge_length)])
-    truckQ = deque(truck_weights)
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
 
-    totalWeight = 0
     totalTime = 0
-
-    while truckQ:
-        totalWeight -= bridgeQ.popleft()
-        if totalWeight + truckQ[0] <= weight:
-            truckWeight = truckQ.popleft()
-            totalWeight += truckWeight
-            bridgeQ.append(truckWeight)
+    bridgeWeight = 0
+    while truck_weights:
+        bridgeWeight -= bridge.popleft()
+        if bridgeWeight + truck_weights[0] <= weight:
+            weightAdd = truck_weights.popleft()
+            bridgeWeight += weightAdd
+            bridge.append(weightAdd)
         else:
-            bridgeQ.append(0)
-
+            bridge.append(0)
         totalTime += 1
 
     totalTime += bridge_length
 
     return totalTime
+
+
+print(solution(2, 10, [7,4,5,6]))
+print(solution(100, 100, [10]))
+print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
