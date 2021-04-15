58d0a44216b8e76c41119d3e327e899cae9c4841
Kimyechan <vlvkcjswo7@naver.com>
2021-04-15 11:41:38 +0900

    programmers binary search resolve

--- programmers/고득점Kit복습/binary-search/입국심사.py
+++ programmers/고득점Kit복습/binary-search/입국심사.py
@@ -1,20 +1,74 @@
-def solution(n, times):
+# def solution(n, times):
+#     start = 1
+#     end = 1000000000 * len(times)
+#
+#     while start < end:
+#         mid = (start + end) // 2
+#
+#         passCount = 0
+#         for i in range(len(times)):
+#             passCount += mid // times[i]
+#
+#         if passCount >= n:
+#             end = mid
+#         else:
+#             start = mid + 1
+#
+#     return end
+#
+#
+# print(solution(6, [7, 10]))
+
+
+def solution(distance, rocks, n):
+    rocks.sort()
+    rocks.append(distance)
+
     start = 1
-    end = 1000000000 * len(times)
+    end = distance
+    result = []
 
     while start < end:
+        location = 0
         mid = (start + end) // 2
+        removeCount = 0
 
-        passCount = 0
-        for i in range(len(times)):
-            passCount += mid // times[i]
+        for rock in rocks:
+            if location + mid > rock:
+                removeCount += 1
+            else:
+                location = rock
 
-        if passCount >= n:
+        if removeCount >= n:
             end = mid
+            if removeCount == n:
+                result.append(end)
         else:
             start = mid + 1
 
-    return end
+    return end - 1
 
 
-print(solution(6, [7, 10]))
+print(solution(25, [2, 14, 11, 21, 17],	2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
