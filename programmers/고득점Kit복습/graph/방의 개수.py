578a601a9ec0f36390b9ccfd6858e2acc831c28c
Kimyechan <vlvkcjswo7@naver.com>
2021-04-15 13:05:50 +0900

    programmers graph resolve

--- programmers/고득점Kit복습/graph/방의 개수.py
+++ programmers/고득점Kit복습/graph/방의 개수.py
@@ -1,41 +1,97 @@
 # check
-from collections import deque, defaultdict
+# from collections import deque, defaultdict
+#
+#
+# def solution(arrows):
+#     arrows.append(4)
+#     answer = 0
+#
+#     q = deque([])
+#     visited = defaultdict(int)
+#     visitedPath = defaultdict(int)
+#
+#     move = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+#
+#     x, y = 0, 0
+#     for arrow in arrows:
+#         for _ in range(2):
+#             x += move[arrow][0]
+#             y += move[arrow][1]
+#             q.append((x, y))
+#
+#     now = (0, 0)
+#     visited[now] = 1
+#
+#     while q:
+#         next = q.popleft()
+#
+#         if visited[next] == 1:
+#             if visitedPath[(now, next)] == 0:
+#                 answer += 1
+#         else:
+#             visited[next] = 1
+#
+#         visitedPath[(now, next)] = 1
+#         visitedPath[(next, now)] = 1
+#         now = next
+#
+#     return answer
+#
+#
+# print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
+from collections import defaultdict
 
 
 def solution(arrows):
-    arrows.append(4)
     answer = 0
+    direction = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
 
-    q = deque([])
-    visited = defaultdict(int)
-    visitedPath = defaultdict(int)
-
-    move = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
-
+    routes = []
     x, y = 0, 0
     for arrow in arrows:
         for _ in range(2):
-            x += move[arrow][0]
-            y += move[arrow][1]
-            q.append((x, y))
+            x += direction[arrow][0]
+            y += direction[arrow][1]
+            routes.append((x, y))
 
-    now = (0, 0)
-    visited[now] = 1
+    visitedNode = defaultdict(int)
+    visitedPath = defaultdict(int)
 
-    while q:
-        next = q.popleft()
+    current = (0, 0)
+    visitedNode[current] = 1
+    for node in routes:
+        if visitedNode[node] == 1 and visitedPath[current, node] != 1:
+            answer += 1
 
-        if visited[next] == 1:
-            if visitedPath[(now, next)] == 0:
-                answer += 1
-        else:
-            visited[next] = 1
-
-        visitedPath[(now, next)] = 1
-        visitedPath[(next, now)] = 1
-        now = next
+        visitedNode[node] = 1
+        visitedPath[current, node] = 1
+        visitedPath[node, current] = 1
+        current = node
 
     return answer
 
 
-print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
+print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0, 4]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- programmers/고득점Kit복습/graph/순위.py
+++ programmers/고득점Kit복습/graph/순위.py
@@ -25,49 +25,117 @@
 #
 #
 # print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
-from collections import defaultdict
+# from collections import defaultdict
+# from collections import deque
+#
+#
+# def solution(n, results):
+#     answer = 0
+#     wins = defaultdict(list) # i가 이긴 사람의 목록
+#     loses = defaultdict(list) # i가 패배한 사람의 목록
+#
+#     for result in results:
+#         wins[result[0]].append(result[1])
+#         loses[result[1]].append(result[0])
+#
+#     for i in range(1, n + 1):
+#         q = deque([i])
+#         visited = [False] * (n + 1)
+#         wCount = 0
+#
+#         while q:
+#             v = q.popleft()
+#             visited[v] = True
+#
+#             for nextP in wins[v]:
+#                 if not visited[nextP]:
+#                     visited[nextP] = True
+#                     wCount += 1
+#                     q.append(nextP)
+#
+#         q = deque([i])
+#         visited = [False] * (n + 1)
+#         lCount = 0
+#
+#         while q:
+#             v = q.popleft()
+#             visited[v] = True
+#
+#             for nextP in loses[v]:
+#                 if not visited[nextP]:
+#                     visited[nextP] = True
+#                     lCount += 1
+#                     q.append(nextP)
+#
+#         if wCount + lCount == n - 1:
+#             answer += 1
+#
+#     return answer
 from collections import deque
 
 
 def solution(n, results):
     answer = 0
-    wins = defaultdict(list) # i가 이긴 사람의 목록
-    loses = defaultdict(list) # i가 패배한 사람의 목록
+    wins = [[] for _ in range(n + 1)]
+    loses = [[] for _ in range(n + 1)]
 
     for result in results:
         wins[result[0]].append(result[1])
         loses[result[1]].append(result[0])
 
     for i in range(1, n + 1):
-        q = deque([i])
-        visited = [False] * (n + 1)
-        wCount = 0
+        start = i
 
+        visited = [False] * (n + 1)
+        visited[i] = True
+        q = deque([start])
+        winCount = 0
         while q:
             v = q.popleft()
-            visited[v] = True
 
-            for nextP in wins[v]:
-                if not visited[nextP]:
-                    visited[nextP] = True
-                    wCount += 1
-                    q.append(nextP)
+            for loser in wins[v]:
+                if not visited[loser]:
+                    visited[loser] = True
+                    winCount += 1
+                    q.append(loser)
 
-        q = deque([i])
         visited = [False] * (n + 1)
-        lCount = 0
-
+        visited[i] = True
+        q = deque([start])
+        loseCount = 0
         while q:
             v = q.popleft()
-            visited[v] = True
 
-            for nextP in loses[v]:
-                if not visited[nextP]:
-                    visited[nextP] = True
-                    lCount += 1
-                    q.append(nextP)
+            for winner in loses[v]:
+                if not visited[winner]:
+                    visited[winner] = True
+                    loseCount += 1
+                    q.append(winner)
 
-        if wCount + lCount == n - 1:
+        if winCount + loseCount == n - 1:
             answer += 1
 
     return answer
+
+
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
