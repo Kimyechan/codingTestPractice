c0ca7e41232e3dd22500b9d58d9246f68cb0f796
Kimyechan <vlvkcjswo7@naver.com>
2021-04-17 00:04:04 +0900

    Baekjun dp 가장 큰 정사각형

---
+++ tony/dp/가장 큰 정사각형.py
@@ -0,0 +1,18 @@
+# check
+n, m = map(int, input().split())
+
+square = [list([0] * (m + 1)) for _ in range(n + 1)]
+dp = [list([0] * (m + 1)) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    temp = list(input())
+    square[i][1:] = list(map(int, temp))
+
+maxLen = 0
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if square[i][j] == 1:
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+            maxLen = max(maxLen, dp[i][j])
+
+print(maxLen ** 2)
