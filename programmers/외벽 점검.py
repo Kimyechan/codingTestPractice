ba5793c9f0fe945f28388c1779b2464db1661bbf
Kimyechan <vlvkcjswo7@naver.com>
2021-04-25 20:01:19 +0900

    programmers 외벽점검 resolve

--- programmers/외벽 점검.py
+++ programmers/외벽 점검.py
@@ -29,27 +29,53 @@
 #
 # print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
 
+# from itertools import permutations
+#
+#
+# def solution(n, weak, dist):
+#     answer = 0
+#     weakPoint = weak + [w + n for w in weak]
+#     candidate = []
+#     for idx, start in enumerate(weak):
+#         for friends in permutations(dist):
+#             count = 1
+#             position = start
+#             for friend in friends:
+#                 position += friend
+#                 if position < weakPoint[idx + len(weak) - 1]:
+#                     count += 1
+#                     for distW in weakPoint[idx + 1: idx + len(weak)]:
+#                         if position < distW:
+#                             position = distW
+#                             break
+#                 else:
+#                     candidate.append(count)
+#                     break
+#
+#     return min(candidate) if candidate else -1
+
 from itertools import permutations
 
 
 def solution(n, weak, dist):
-    answer = 0
-    weakPoint = weak + [w + n for w in weak]
-    candidate = []
-    for idx, start in enumerate(weak):
-        for friends in permutations(dist):
+    answer = []
+    weakExtend = weak + [w + n for w in weak]
+
+    for friends in permutations(dist):
+        for weakIndex in range(len(weak)):
+            position = weak[weakIndex]
             count = 1
-            position = start
             for friend in friends:
-                position += friend
-                if position < weakPoint[idx + len(weak) - 1]:
+                if position + friend < weakExtend[weakIndex + len(weak) - 1]:
                     count += 1
-                    for distW in weakPoint[idx + 1: idx + len(weak)]:
-                        if position < distW:
-                            position = distW
+                    for i in range(weakIndex, weakIndex + len(weak)):
+                        if position + friend < weakExtend[i]:
+                            position = weakExtend[i]
                             break
                 else:
-                    candidate.append(count)
-                    break
+                    answer.append(count)
 
-    return min(candidate) if candidate else -1
+    if answer:
+        return min(answer)
+    else:
+        return -1
