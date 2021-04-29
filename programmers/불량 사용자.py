a48c806a18ae110cccd6458d28e409708699d62b
Kimyechan <vlvkcjswo7@naver.com>
2021-04-29 14:59:14 +0900

    programmers 불량 사용자 resolve

--- programmers/불량 사용자.py
+++ programmers/불량 사용자.py
@@ -22,57 +22,86 @@
 #             if com_set not in ans:
 #                 ans.append(com_set)
 #     return len(ans)
-from copy import deepcopy
-
-bannedList = []
 
 
-def checkCandidate(userId, id):
-    result = []
-    for cand in userId:
-        if len(cand) != len(id):
-            continue
-        correctFlag = True
-        for idx, c in enumerate(list(cand)):
-            if id[idx] == "*":
+# from copy import deepcopy
+#
+# bannedList = []
+#
+#
+# def checkCandidate(userId, id):
+#     result = []
+#     for cand in userId:
+#         if len(cand) != len(id):
+#             continue
+#         correctFlag = True
+#         for idx, c in enumerate(list(cand)):
+#             if id[idx] == "*":
+#                 continue
+#             if id[idx] != c:
+#                 correctFlag = False
+#                 break
+#         if correctFlag:
+#             result.append(cand)
+#
+#     return result
+#
+#
+# def calcBannedList(user_id, banned_id, candList, index, candidate, visited):
+#     if index == len(banned_id):
+#         if set(candList) not in bannedList:
+#             bannedList.append(deepcopy(set(candList)))
+#         return
+#
+#     for cand in candidate[index]:
+#         if not visited[cand]:
+#             candList.append(cand)
+#             visited[cand] = True
+#             calcBannedList(user_id, banned_id, candList, index + 1, candidate, visited)
+#             print(candList)
+#             visited[cand] = False
+#             candList.pop()
+#
+#
+# def solution(user_id, banned_id):
+#     candidate = []
+#     for id in banned_id:
+#         candidate.append(checkCandidate(user_id, id))
+#
+#     visited = dict()
+#     for id in user_id:
+#         visited[id] = False
+#
+#     calcBannedList(user_id, banned_id, [], 0, candidate, visited)
+#     print(bannedList)
+#     return len(bannedList)
+
+
+from itertools import permutations
+
+
+def checkBanned(userPerm, banned_id):
+    for i in range(len(userPerm)):
+        if len(userPerm[i]) != len(banned_id[i]):
+            return False
+        for j in range(len(userPerm[i])):
+            if banned_id[i][j] == "*":
                 continue
-            if id[idx] != c:
-                correctFlag = False
-                break
-        if correctFlag:
-            result.append(cand)
+            if banned_id[i][j] != userPerm[i][j]:
+                return False
 
-    return result
-
-
-def calcBannedList(user_id, banned_id, candList, index, candidate, visited):
-    if index == len(banned_id):
-        if set(candList) not in bannedList:
-            bannedList.append(deepcopy(set(candList)))
-        return
-
-    for cand in candidate[index]:
-        if not visited[cand]:
-            candList.append(cand)
-            visited[cand] = True
-            calcBannedList(user_id, banned_id, candList, index + 1, candidate, visited)
-            print(candList)
-            visited[cand] = False
-            candList.pop()
+    return True
 
 
 def solution(user_id, banned_id):
-    candidate = []
-    for id in banned_id:
-        candidate.append(checkCandidate(user_id, id))
+    answer = []
+    for userPerm in permutations(user_id, len(banned_id)):
+        if checkBanned(userPerm, banned_id):
+            temp = set(userPerm)
+            if temp not in answer:
+                answer.append(temp)
 
-    visited = dict()
-    for id in user_id:
-        visited[id] = False
-
-    calcBannedList(user_id, banned_id, [], 0, candidate, visited)
-    print(bannedList)
-    return len(bannedList)
+    return len(answer)
 
 
 # print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
