e84a2e92b17302bba55be6ffc73b7bb56ae8da79
Kimyechan <vlvkcjswo7@naver.com>
2021-04-30 17:48:12 +0900

    programmers 야근 지수

---
+++ programmers/야근 지수.py
@@ -0,0 +1,22 @@
+import heapq
+
+
+def solution(n, works):
+    answer = 0
+    hq = []
+    for work in works:
+        heapq.heappush(hq, (-work, work))
+
+    while n > 0:
+        work = heapq.heappop(hq)
+        if work[1] <= 0:
+            break
+
+        newWork = work[1] - 1
+        heapq.heappush(hq, (-newWork, newWork))
+        n -= 1
+
+    for work in list(hq):
+        answer += work[1] ** 2
+
+    return answer
