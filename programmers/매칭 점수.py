114beeee090cfe76594afd9f3df0beea49b08cb1
Kimyechan <vlvkcjswo7@naver.com>
2021-05-12 23:52:30 +0900

    programmers 매칭 점수 resolve

--- programmers/매칭 점수.py
+++ programmers/매칭 점수.py
@@ -1,4 +1,4 @@
-# check
+# check #check
 import re
 from copy import deepcopy
 
@@ -74,6 +74,44 @@ def solution(word, pages):
 #
 #     result.sort(key=lambda x: (-x[1], x[0]))
 #
+#     return result[0][0]
+
+
+# import re
+# from copy import deepcopy
+#
+#
+# def solution(word, pages):
+#     basicScore = dict()
+#     linkList = dict()
+#     pageIndex = dict()
+#
+#     for idx, page in enumerate(pages):
+#         p1 = re.compile('<meta property="og:url" content="(.*?)"/>')
+#         metaContent = p1.findall(page)
+#
+#         p2 = re.compile('<a href="(.*?)">')
+#         links = p2.findall(page)
+#
+#         count = re.sub('[^a-zA-Z]', ' ', page).lower().split().count(word.lower())
+#
+#         basicScore[metaContent[0]] = count
+#         linkList[metaContent[0]] = links
+#         pageIndex[metaContent[0]] = idx
+#
+#     totalScore = deepcopy(basicScore)
+#     for key, links in linkList.items():
+#         for link in links:
+#             if basicScore.get(link) is None:
+#                 continue
+#             totalScore[link] += basicScore[key] / len(links)
+#
+#     result = []
+#     for key, score in totalScore.items():
+#         result.append((pageIndex[key], score))
+#     result.sort(key=lambda x: (-x[1], x[0]))
+#
+#     print(result)
 #     return result[0][0]
 
 print(solution('blind', ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
