410f715812e7f8a5e3482dd802d7906ea41e6a06
Kimyechan <vlvkcjswo7@naver.com>
2021-05-12 22:37:34 +0900

    programmers 징검다리 건너기

--- programmers/징검다리 건너기.py
+++ programmers/징검다리 건너기.py
@@ -1,4 +1,31 @@
 # check
+# def solution(stones, k):
+#     start = 1
+#     end = max(stones)
+#
+#     while start < end:
+#         mid = (start + end) // 2
+#
+#         stonesTemp = stones[:]
+#         count = 0
+#         flag = False
+#         for stone in stonesTemp:
+#             if stone - mid <= 0:
+#                 count += 1
+#             else:
+#                 count = 0
+#             if count == k:
+#                 flag = True
+#                 break
+#
+#         if flag:
+#             end = mid
+#         else:
+#             start = mid + 1
+#
+#     return end
+
+
 def solution(stones, k):
     start = 1
     end = max(stones)
@@ -6,19 +33,18 @@ def solution(stones, k):
     while start < end:
         mid = (start + end) // 2
 
-        stonesTemp = stones[:]
         count = 0
-        flag = False
-        for stone in stonesTemp:
+        isPossible = True
+        for stone in stones:
             if stone - mid <= 0:
                 count += 1
             else:
                 count = 0
             if count == k:
-                flag = True
+                isPossible = False
                 break
 
-        if flag:
+        if not isPossible:
             end = mid
         else:
             start = mid + 1
