0bf5e669718d99c34c4ea76cd40ba968c2ed3c2b
Kimyechan <vlvkcjswo7@naver.com>
2021-05-14 10:18:33 +0900

    programmers 풍선 터트리기

---
+++ programmers/스타 수열.py
@@ -0,0 +1,52 @@
+# check
+# from collections import Counter
+#
+#
+# def solution(a):
+#     answer = -1
+#     elements = Counter(a)
+#
+#     for k in elements.keys():
+#         if elements[k] <= answer:
+#             continue
+#         commonCnt = 0
+#         idx = 0
+#         while idx < len(a) - 1:
+#             if (a[idx] != k and a[idx + 1] != k) or (a[idx] == a[idx + 1]):
+#                 idx += 1
+#                 continue
+#             commonCnt += 1
+#             idx += 2
+#         answer = max(commonCnt, answer)
+#
+#     if answer == -1:
+#         return 0
+#     else:
+#         return answer * 2
+
+from collections import Counter
+
+
+def solution(a):
+    answer = -1
+    numCount = Counter(a)
+
+    for k in numCount.keys():
+        if numCount[k] <= answer:
+            continue
+
+        starSeq = 0
+        idx = 0
+        while idx < len(a) - 1:
+            if (a[idx] != k and a[idx + 1] != k) or (a[idx] == a[idx + 1]):
+                idx += 1
+                continue
+            starSeq += 1
+            idx += 2
+
+        answer = max(answer, starSeq)
+
+    if answer == -1:
+        return 0
+    else:
+        return answer * 2
--- programmers/풍선 터트리기.py
+++ programmers/풍선 터트리기.py
@@ -1,26 +1,49 @@
 # check
+# def solution(a):
+#     answer = 0
+#     minLeft = [float('inf')] * len(a)
+#     minRight = [float('inf')] * len(a)
+#
+#     leftMinValue = float('inf')
+#     for i in range(len(a)):
+#         if leftMinValue > a[i]:
+#             leftMinValue = a[i]
+#         minLeft[i] = leftMinValue
+#
+#     rightMinValue = float('inf')
+#     for i in range(len(a) - 1, -1, -1):
+#         if rightMinValue > a[i]:
+#             rightMinValue = a[i]
+#         minRight[i] = rightMinValue
+#
+#     for i in range(len(a)):
+#         if a[i] <= minLeft[i] or a[i] <= minRight[i]:
+#             answer += 1
+#
+#     return answer
+
+
 def solution(a):
     answer = 0
-    minLeft = [float('inf')] * len(a)
-    minRight = [float('inf')] * len(a)
+    minL = [0] * len(a)
+    minR = [0] * len(a)
 
-    leftMinValue = float('inf')
+    minLV = float('inf')
     for i in range(len(a)):
-        if leftMinValue > a[i]:
-            leftMinValue = a[i]
-        minLeft[i] = leftMinValue
+        if minLV > a[i]:
+            minLV = a[i]
+        minL[i] = minLV
 
-    rightMinValue = float('inf')
+    minRV = float('inf')
     for i in range(len(a) - 1, -1, -1):
-        if rightMinValue > a[i]:
-            rightMinValue = a[i]
-        minRight[i] = rightMinValue
+        if minRV > a[i]:
+            minRV = a[i]
+        minR[i] = minRV
 
     for i in range(len(a)):
-        if a[i] <= minLeft[i] or a[i] <= minRight[i]:
+        if minL[i] >= a[i] or minR[i] >= a[i]:
             answer += 1
 
     return answer
-
 
 print(solution([9, -1, -5]))
