fd0454cac4c15ff92c7b202acbc5a5d2ba7ef1d2
Kimyechan <vlvkcjswo7@naver.com>
2021-05-15 20:33:40 +0900

    programmers 기지국 resolve

--- programmers/기지국 설치.py
+++ programmers/기지국 설치.py
@@ -18,3 +18,24 @@ def solution(n, stations, w):
         answer += math.ceil(distance / (w * 2 + 1))
 
     return answer
+
+
+# import math
+#
+#
+# def solution(n, stations, w):
+#     answer = 0
+#
+#     distances = []
+#     for i in range(1, len(stations)):
+#         distances.append((stations[i] - w) - (stations[i - 1] + w) - 1)
+#
+#     distances.append(stations[0] - w - 1)
+#     distances.append(n - (stations[-1] + w))
+#
+#     for distance in distances:
+#         if distance <= 0:
+#             continue
+#         answer += math.ceil(distance / (2 * w + 1))
+#
+#     return answer
