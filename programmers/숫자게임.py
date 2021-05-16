950a7e12b253aeacd386fb1daaeb3eb67928c83e
Kimyechan <vlvkcjswo7@naver.com>
2021-05-16 17:53:08 +0900

    programmers 숫자게임

--- programmers/숫자게임.py
+++ programmers/숫자게임.py
@@ -26,17 +26,33 @@
 #     return answer
 
 
+# def solution(A, B):
+#     answer = 0
+#     A.sort()
+#     B.sort()
+#     j = 0
+#
+#     for i in range(len(A)):
+#         if A[j] < B[i]:
+#             answer = answer + 1
+#             j = j + 1
+#
+#     return answer
+
+
 def solution(A, B):
     answer = 0
     A.sort()
     B.sort()
-    j = 0
 
-    for i in range(len(A)):
+    n = len(A)
+    j = 0
+    for i in range(n):
         if A[j] < B[i]:
-            answer = answer + 1
-            j = j+1
+            answer += 1
+            j += 1
 
     return answer
+
 
 print(solution([5, 1, 3, 7], [2, 2, 6, 8]))
