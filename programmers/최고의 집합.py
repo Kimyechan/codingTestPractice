9639c94c47fa11aa0d59aca4a286e66d766ee5e2
Kimyechan <vlvkcjswo7@naver.com>
2021-05-17 23:32:00 +0900

    programmers 최고의 집합 resolve

--- programmers/최고의 집합.py
+++ programmers/최고의 집합.py
@@ -14,20 +14,35 @@
 #     return answer
 
 
-def solution(n, k):
+# def solution(n, k):
+#     answer = []
+#
+#     numbers = [i for i in range(1, n + 1)]
+#     factorial = []
+#     num = 1
+#     for i in range(1, n):
+#         num *= i
+#         factorial.append(num)
+#
+#     for i in range(1, n):
+#         idx = (k - 1) // factorial[n - i - 1]
+#         k = k % factorial[n - i - 1]
+#         answer.append(numbers.pop(idx))
+#
+#     answer.append(numbers[0])
+#     return answer
+
+
+def solution(n, s):
     answer = []
+    div = s // n
+    mod = s % n
 
-    numbers = [i for i in range(1, n + 1)]
-    factorial = []
-    num = 1
-    for i in range(1, n):
-        num *= i
-        factorial.append(num)
+    if div == 0:
+        return [-1]
 
-    for i in range(1, n):
-        idx = (k - 1) // factorial[n - i - 1]
-        k = k % factorial[n - i - 1]
-        answer.append(numbers.pop(idx))
+    answer = [div] * n
+    for i in range(n - 1, n - mod - 1, - 1):
+        answer[i] += 1
 
-    answer.append(numbers[0])
     return answer
