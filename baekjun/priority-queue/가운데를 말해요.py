7b365f4af7da9d29c0f6157eb73ec97a3586ea48
Kimyechan <vlvkcjswo7@naver.com>
2021-05-22 23:57:49 +0900

    baekjun 가운데를 말해요 resolve

--- baekjun/priority-queue/가운데를 말해요.py
+++ baekjun/priority-queue/가운데를 말해요.py
@@ -1,11 +1,35 @@
-import heapq
-import sys
+# check
+# import heapq
+# import sys
+#
+# n = int(input())
+# left = []
+# right = []
+#
+# for _ in range(n):
+#     num = int(sys.stdin.readline())
+#
+#     if len(left) == len(right):
+#         heapq.heappush(left, (-num, num))
+#     else:
+#         heapq.heappush(right, (num, num))
+#
+#     if right and left[0][1] > right[0][1]:
+#         leftMax = heapq.heappop(left)[1]
+#         rightMin = heapq.heappop(right)[1]
+#         heapq.heappush(left, (-rightMin, rightMin))
+#         heapq.heappush(right, (leftMax, leftMax))
+#
+#     print(left[0][1])
 
-n = int(input())
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
 left = []
 right = []
 
-for _ in range(n):
+for i in range(n):
     num = int(sys.stdin.readline())
 
     if len(left) == len(right):
@@ -20,3 +44,28 @@ for _ in range(n):
         heapq.heappush(right, (leftMax, leftMax))
 
     print(left[0][1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
