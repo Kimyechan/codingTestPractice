f1bfb1aac837d18af91e93b68ed23e23eea250bb
Kimyechan <vlvkcjswo7@naver.com>
2021-05-29 00:04:25 +0900

    baekjun 생태학

---
+++ baekjun/생태학.py
@@ -0,0 +1,19 @@
+import sys
+from collections import defaultdict
+
+treeSet = set()
+treeDict = defaultdict(int)
+totalCount = 0
+while True:
+    tree = sys.stdin.readline().rstrip()
+    if tree == "":
+        break
+    treeDict[tree] += 1
+    treeSet.add(tree)
+    totalCount += 1
+
+treeList = list(treeSet)
+treeList.sort()
+for tree in treeList:
+    percent = treeDict[tree] * 100 / totalCount
+    print("%s %0.4f" %(tree, percent))
