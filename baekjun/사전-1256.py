ae59813bed4700f83265906cd34a3a5429de34d2
Kimyechan <vlvkcjswo7@naver.com>
2021-06-03 14:24:43 +0900

    Resolve: baekjun 사전 1256

--- baekjun/사전-1256.py
+++ baekjun/사전-1256.py
@@ -1,24 +1,55 @@
 # check
+# import sys
+#
+# n, m, k = map(int, sys.stdin.readline().split())
+# arr = [[1] * (m + 1) for _ in range(n + 1)]
+#
+# for i in range(1, n + 1):
+#     for j in range(1, m + 1):
+#         arr[i][j] = arr[i - 1][j] + arr[i][j - 1]  # a로 시작 or z로 시작
+#
+# if arr[n][m] < k:
+#     print(-1)
+# else:
+#     result = ""
+#     while True:
+#         if n == 0 or m == 0:
+#             result += "z" * m
+#             result += "a" * n
+#             break
+#
+#         flag = arr[n - 1][m]
+#         if k <= flag:
+#             result += "a"
+#             n -= 1
+#         else:
+#             result += "z"
+#             m -= 1
+#             k -= flag
+#     print(result)
+
 import sys
 
 n, m, k = map(int, sys.stdin.readline().split())
-arr = [[1] * (m + 1) for _ in range(n + 1)]
+
+az = [[1] * (m + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, m + 1):
-        arr[i][j] = arr[i - 1][j] + arr[i][j - 1]  # a로 시작 or z로 시작
+        az[i][j] = az[i - 1][j] + az[i][j - 1]
 
-if arr[n][m] < k:
+if az[n][m] < k:
     print(-1)
 else:
     result = ""
+
     while True:
         if n == 0 or m == 0:
-            result += "z" * m
             result += "a" * n
+            result += "z" * m
             break
 
-        flag = arr[n - 1][m]
+        flag = az[n - 1][m]
         if k <= flag:
             result += "a"
             n -= 1
@@ -26,4 +57,18 @@ else:
             result += "z"
             m -= 1
             k -= flag
+
     print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
